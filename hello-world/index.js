8bba4b11941ae54a39452a6629e5a189bb428631
irfan baig <ibaig9356@gmail.com>
2025-12-31 17:44:38 +0530

    feat: Run the simple js with some code

---
+++ hello-world/index.js
@@ -0,0 +1,16 @@
+console.log("hellow world");
+
+const array = [1, 2, 3, 4, 5];
+
+console.log("This is an Array : ", array);
+
+setTimeout(() => {
+    console.log("After 2 sec");    
+}, 2000);
+
+console.log("After completing sync proccess");
+
+
+
+
+
