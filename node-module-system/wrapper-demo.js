fd0ce9964a1794f98b2bed2f4c1118993a45d7cd
irfan baig <ibaig9356@gmail.com>
2026-01-01 08:21:01 +0530

    feat : node module system

---
+++ node-module-system/wrapper-demo.js
@@ -0,0 +1,10 @@
+const wrraperExp = require("./wrapper-explorer.js") 
+
+console.log(`in wrapper demo.js file`);
+
+console.log('__filename inside demo ', __filename);
+console.log('__dirname inside demo ', __dirname);
+
+wrraperExp.greet("irfaaaaaa")
+  
+
